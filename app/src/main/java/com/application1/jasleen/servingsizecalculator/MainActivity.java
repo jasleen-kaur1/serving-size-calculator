f57697705b2060fd8bad2f13ebbec0a2c895487a
Jasleen Kaur <jka185@sfu.ca>
2018-05-18 14:26:43 -0700

    Initial commit

---
+++ app/src/main/java/com/application1/jasleen/servingsizecalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.application1.jasleen.servingsizecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//initial
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
